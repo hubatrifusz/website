26f37c485346221f0da5884c4fc891f1849e29ac
Huba Trifusz <huba.trifusz@gmail.com>
2026-06-22 13:46:50 +0200

    fix: moved dir alias to the correct file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vitest/config'
 import { defineVitestProject } from '@nuxt/test-utils/config'
+import { resolve } from 'node:path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~~': resolve(__dirname),
+      '@@': resolve(__dirname),
+    },
+  },
   test: {
     projects: [
       {
